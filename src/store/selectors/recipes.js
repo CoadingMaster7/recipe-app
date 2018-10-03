fce3741851c49cd57057634f9f941c0ffba732a7
Paulina Masiak <paula.masiak@gmail.com>
2018-10-03 11:05:20 +0200

    Performance improvement - add reselect

--- src/store/selectors/recipes.js
+++ src/store/selectors/recipes.js
@@ -1,7 +1,15 @@
-export const getAllRecipes = (state) => {
-  const { recipes } = state;
-  return recipes.allIds.map((recipeId) => recipes.byId[recipeId]);
-};
+import { createSelector } from 'reselect'
+
+const getRecipesIds = (state) => state.recipes.allIds;
+const getRecipesById = (state) => state.recipes.byId;
+
+export const getAllRecipes = createSelector(
+  getRecipesIds,
+  getRecipesById,
+  (recipesIds, recipesById) => (
+    recipesIds.map((recipeId) => recipesById[recipeId])
+  )
+);
 
 export const getRecipeById = (state, recipeId) => {
   const { recipes } = state;
